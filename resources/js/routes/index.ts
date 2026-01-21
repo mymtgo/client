8c04a20118e02e88b7d4b9f9f0de692e000ef215
Alec Ritson <hello@itsalec.co.uk>
2026-01-21 11:14:00 +0000

    Tweaks

--- resources/js/routes/index.ts
+++ resources/js/routes/index.ts
@@ -1,48 +1,9 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition } from './../wayfinder'
 /**
- * @see routes/web.php:7
- * @route '/setup'
- */
-export const setup = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
-    url: setup.url(options),
-    method: 'get',
-})
-
-setup.definition = {
-    methods: ["get","head"],
-    url: '/setup',
-} satisfies RouteDefinition<["get","head"]>
-
-/**
- * @see routes/web.php:7
- * @route '/setup'
- */
-setup.url = (options?: RouteQueryOptions) => {
-    return setup.definition.url + queryParams(options)
-}
-
-/**
- * @see routes/web.php:7
- * @route '/setup'
- */
-setup.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
-    url: setup.url(options),
-    method: 'get',
-})
-/**
- * @see routes/web.php:7
- * @route '/setup'
- */
-setup.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
-    url: setup.url(options),
-    method: 'head',
-})
-
-/**
 * @see \App\Http\Controllers\IndexController::__invoke
- * @see app/Http/Controllers/IndexController.php:20
- * @route '/'
- */
+* @see app/Http/Controllers/IndexController.php:20
+* @route '/'
+*/
 export const home = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
     url: home.url(options),
     method: 'get',
@@ -55,28 +16,30 @@ home.definition = {
 
 /**
 * @see \App\Http\Controllers\IndexController::__invoke
- * @see app/Http/Controllers/IndexController.php:20
- * @route '/'
- */
+* @see app/Http/Controllers/IndexController.php:20
+* @route '/'
+*/
 home.url = (options?: RouteQueryOptions) => {
     return home.definition.url + queryParams(options)
 }
 
 /**
 * @see \App\Http\Controllers\IndexController::__invoke
- * @see app/Http/Controllers/IndexController.php:20
- * @route '/'
- */
+* @see app/Http/Controllers/IndexController.php:20
+* @route '/'
+*/
 home.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
     url: home.url(options),
     method: 'get',
 })
+
 /**
 * @see \App\Http\Controllers\IndexController::__invoke
- * @see app/Http/Controllers/IndexController.php:20
- * @route '/'
- */
+* @see app/Http/Controllers/IndexController.php:20
+* @route '/'
+*/
 home.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
     url: home.url(options),
     method: 'head',
 })
+
