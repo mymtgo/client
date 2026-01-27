2829c4bb6e11db06551ae4eb19c7b6c98a900a71
Alec Ritson <hello@itsalec.co.uk>
2026-01-27 12:58:06 +0000

    WIP

--- resources/js/Pages/decks/partials/MatchHistoryChart.vue
+++ resources/js/Pages/decks/partials/MatchHistoryChart.vue
@@ -2,75 +2,47 @@
 import type { ChartConfig } from '@/components/ui/chart';
 
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+
+const props = defineProps<{
+    data: any[];
+}>();
+
 import { ChartContainer } from '@/components/ui/chart';
 import { VisAxis, VisLine, VisXYContainer } from '@unovis/vue';
 import { TrendingUp } from 'lucide-vue-next';
+import { map } from 'lodash-es';
 
-const description = 'A line chart';
+type Data = { day: Date; rate: number };
 
-const chartData = [
-    { x: 0, y: 0 },
-    { x: 1, y: 2 },
-    { x: 2, y: 1 },
-];
+const chartData: Data[] = map(props.data, (data: any) => {
+    return {
+        date: new Date(data.date),
+        rate: parseInt(data.winrate),
+    };
+});
 
 const chartConfig = {
-    desktop: {
-        label: 'Desktop',
+    rate: {
+        label: 'Winrate',
         color: 'var(--chart-1)',
     },
 } satisfies ChartConfig;
+
+const formatDay = (ms: number) => new Date(ms).toLocaleDateString('en-GB', { weekday: 'short' });
 </script>
 
 <template>
-    <Card>
-        <CardHeader>
-            <CardTitle>Deck winrate</CardTitle>
-            <CardDescription>Last 7 days</CardDescription>
-        </CardHeader>
-        <CardContent>
-            <ChartContainer :config="chartConfig" class="h-64">
-                <VisXYContainer>
-                    <VisLine :data="chartData" :x="(d) => d.x" :y="(d) => d.y" />
-                    <VisAxis type="x" />
-                    <VisAxis type="y" label="Foo" />
-                </VisXYContainer>
-                <!--                <VisXYContainer :data="chartData" :margin="{ left: -24 }" :y-domain="[0, undefined]">-->
-                <!--                    <VisLine-->
-                <!--                        :x="(d: Data) => d.date"-->
-                <!--                        :y="(d: Data) => d.desktop"-->
-                <!--                        :color="chartConfig.desktop.color"-->
-                <!--                        :curve-type="CurveType.Natural"-->
-                <!--                    />-->
-                <!--                    <VisAxis-->
-                <!--                        type="x"-->
-                <!--                        :x="(d: Data) => d.date"-->
-                <!--                        :tick-line="false"-->
-                <!--                        :domain-line="false"-->
-                <!--                        :grid-line="false"-->
-                <!--                        :num-ticks="6"-->
-                <!--                        :tick-format="-->
-                <!--                            (d: number) => {-->
-                <!--                                const date = new Date(d);-->
-                <!--                                return date.toLocaleDateString('en-US', {-->
-                <!--                                    month: 'short',-->
-                <!--                                });-->
-                <!--                            }-->
-                <!--                        "-->
-                <!--                        :tick-values="chartData.map((d) => d.date)"-->
-                <!--                    />-->
-                <!--                    <VisAxis type="y" :num-ticks="3" :tick-line="false" :domain-line="false" />-->
-                <!--                    <ChartTooltip />-->
-                <!--                    <ChartCrosshair-->
-                <!--                        :template="componentToString(chartConfig, ChartTooltipContent, { hideLabel: true })"-->
-                <!--                        :color="chartConfig.desktop.color"-->
-                <!--                    />-->
-                <!--                </VisXYContainer>-->
-            </ChartContainer>
-        </CardContent>
-        <CardFooter class="flex-col items-start gap-2 text-sm">
-            <div class="flex gap-2 leading-none font-medium">Trending up by 5.2% this month <TrendingUp class="h-4 w-4" /></div>
-            <div class="leading-none text-muted-foreground">Showing total visitors for the last 6 months</div>
-        </CardFooter>
-    </Card>
+    <div>
+        <header>
+            <strong>Deck winrate</strong>
+        </header>
+        <!-- Size is IMPORTANT -->
+        <ChartContainer :config="chartConfig" class="h-100 w-full mt-4">
+            <VisXYContainer :data="chartData">
+                <VisLine :x="(d) => d.date" :y="(d) => d.rate" :color="chartConfig.rate.color" />
+                <VisAxis type="x" label="Day" :tick-format="formatDay" />
+                <VisAxis type="y" label="Winrate %" />
+            </VisXYContainer>
+        </ChartContainer>
+    </div>
 </template>
