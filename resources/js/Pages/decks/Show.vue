9b36b2635ed6bef9798528c92e407d2385d25457
Alec Ritson <hello@itsalec.co.uk>
2026-01-27 12:58:06 +0000

    WIP

--- resources/js/Pages/decks/Show.vue
+++ resources/js/Pages/decks/Show.vue
@@ -11,6 +11,7 @@ import MatchupSpread from '@/Pages/decks/partials/MatchupSpread.vue';
 import DeckMatches from '@/Pages/decks/partials/DeckMatches.vue';
 import DeckLeagues from '@/Pages/decks/partials/DeckLeagues.vue';
 import DeckList from '@/Pages/decks/partials/DeckList.vue';
+import MatchHistoryChart from '@/Pages/decks/partials/MatchHistoryChart.vue';
 
 defineProps<{
     matchupSpread: any[];
@@ -32,7 +33,8 @@ defineProps<{
     gamesOtpLost: number;
     otpRate: number;
     matches: App.Data.Front.MatchData[];
-    leagues: App.Data.Front.LeagueData[]
+    leagues: App.Data.Front.LeagueData[];
+    matchChartData: any[];
 }>();
 
 usePoll(2000);
@@ -50,6 +52,7 @@ usePoll(2000);
                         <option value="year">This year</option>
                     </NativeSelect>
                 </div>
+
                 <div class="grid grid-cols-4 gap-4">
                     <Card class="gap-0">
                         <CardHeader>Total Matches</CardHeader>
@@ -72,6 +75,8 @@ usePoll(2000);
                         <CardContent class="text-xl"> {{ gameWinrate }}%</CardContent>
                     </Card>
                 </div>
+
+                <MatchHistoryChart :data="matchChartData" />
 
                 <div class="grid grid-cols-4 gap-4">
                     <Card class="gap-0">
@@ -104,12 +109,11 @@ usePoll(2000);
                         <TabsContent value="matchupSpread">
                             <MatchupSpread :matchupSpread="matchupSpread" />
                         </TabsContent>
-
                     </Tabs>
                 </div>
             </div>
 
-            <div class="col-span-3 px-8 h-screen no-scrollbar overflow-y-auto">
+            <div class="col-span-3 no-scrollbar h-screen overflow-y-auto px-8">
                 <DeckList :maindeck="maindeck" :sideboard="sideboard" />
             </div>
         </div>
