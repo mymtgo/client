9d2045f25e79e70623c6c952a7c14952a2081333
Alec Ritson <hello@itsalec.co.uk>
2026-01-28 14:19:13 +0000

    wip

--- resources/js/actions/App/Http/Controllers/Matches/DeleteController.ts
+++ resources/js/actions/App/Http/Controllers/Matches/DeleteController.ts
@@ -1,9 +1,9 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, applyUrlDefaults } from './../../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\Matches\DeleteController::__invoke
- * @see app/Http/Controllers/Matches/DeleteController.php:11
- * @route '/matches/{id}'
- */
+* @see app/Http/Controllers/Matches/DeleteController.php:11
+* @route '/matches/{id}'
+*/
 const DeleteController = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'delete'> => ({
     url: DeleteController.url(args, options),
     method: 'delete',
@@ -16,26 +16,25 @@ DeleteController.definition = {
 
 /**
 * @see \App\Http\Controllers\Matches\DeleteController::__invoke
- * @see app/Http/Controllers/Matches/DeleteController.php:11
- * @route '/matches/{id}'
- */
+* @see app/Http/Controllers/Matches/DeleteController.php:11
+* @route '/matches/{id}'
+*/
 DeleteController.url = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions) => {
     if (typeof args === 'string' || typeof args === 'number') {
         args = { id: args }
     }
 
-    
     if (Array.isArray(args)) {
         args = {
-                    id: args[0],
-                }
+            id: args[0],
+        }
     }
 
     args = applyUrlDefaults(args)
 
     const parsedArgs = {
-                        id: args.id,
-                }
+        id: args.id,
+    }
 
     return DeleteController.definition.url
             .replace('{id}', parsedArgs.id.toString())
@@ -44,11 +43,12 @@ DeleteController.url = (args: { id: string | number } | [id: string | number ] |
 
 /**
 * @see \App\Http\Controllers\Matches\DeleteController::__invoke
- * @see app/Http/Controllers/Matches/DeleteController.php:11
- * @route '/matches/{id}'
- */
+* @see app/Http/Controllers/Matches/DeleteController.php:11
+* @route '/matches/{id}'
+*/
 DeleteController.delete = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'delete'> => ({
     url: DeleteController.url(args, options),
     method: 'delete',
 })
+
 export default DeleteController
--- resources/js/actions/Native/Desktop/Http/Controllers/CreateSecurityCookieController.ts
+++ resources/js/actions/Native/Desktop/Http/Controllers/CreateSecurityCookieController.ts
@@ -1,9 +1,9 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition } from './../../../../../wayfinder'
 /**
 * @see \Native\Desktop\Http\Controllers\CreateSecurityCookieController::__invoke
- * @see vendor/nativephp/desktop/src/Http/Controllers/CreateSecurityCookieController.php:9
- * @route '/_native/api/cookie'
- */
+* @see vendor/nativephp/desktop/src/Http/Controllers/CreateSecurityCookieController.php:9
+* @route '/_native/api/cookie'
+*/
 const CreateSecurityCookieController = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
     url: CreateSecurityCookieController.url(options),
     method: 'get',
@@ -16,29 +16,31 @@ CreateSecurityCookieController.definition = {
 
 /**
 * @see \Native\Desktop\Http\Controllers\CreateSecurityCookieController::__invoke
- * @see vendor/nativephp/desktop/src/Http/Controllers/CreateSecurityCookieController.php:9
- * @route '/_native/api/cookie'
- */
+* @see vendor/nativephp/desktop/src/Http/Controllers/CreateSecurityCookieController.php:9
+* @route '/_native/api/cookie'
+*/
 CreateSecurityCookieController.url = (options?: RouteQueryOptions) => {
     return CreateSecurityCookieController.definition.url + queryParams(options)
 }
 
 /**
 * @see \Native\Desktop\Http\Controllers\CreateSecurityCookieController::__invoke
- * @see vendor/nativephp/desktop/src/Http/Controllers/CreateSecurityCookieController.php:9
- * @route '/_native/api/cookie'
- */
+* @see vendor/nativephp/desktop/src/Http/Controllers/CreateSecurityCookieController.php:9
+* @route '/_native/api/cookie'
+*/
 CreateSecurityCookieController.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
     url: CreateSecurityCookieController.url(options),
     method: 'get',
 })
+
 /**
 * @see \Native\Desktop\Http\Controllers\CreateSecurityCookieController::__invoke
- * @see vendor/nativephp/desktop/src/Http/Controllers/CreateSecurityCookieController.php:9
- * @route '/_native/api/cookie'
- */
+* @see vendor/nativephp/desktop/src/Http/Controllers/CreateSecurityCookieController.php:9
+* @route '/_native/api/cookie'
+*/
 CreateSecurityCookieController.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
     url: CreateSecurityCookieController.url(options),
     method: 'head',
 })
+
 export default CreateSecurityCookieController
