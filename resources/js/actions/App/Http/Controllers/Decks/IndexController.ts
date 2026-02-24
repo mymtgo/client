580b86ce489fb1aec104a3531b0ca0e96a2cf392
Alec Ritson <hello@itsalec.co.uk>
2026-02-24 12:26:46 +0000

    wip

--- resources/js/actions/App/Http/Controllers/Decks/IndexController.ts
+++ resources/js/actions/App/Http/Controllers/Decks/IndexController.ts
@@ -1,9 +1,9 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\Decks\IndexController::__invoke
- * @see app/Http/Controllers/Decks/IndexController.php:12
- * @route '/decks'
- */
+* @see app/Http/Controllers/Decks/IndexController.php:12
+* @route '/decks'
+*/
 const IndexController = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
     url: IndexController.url(options),
     method: 'get',
@@ -16,65 +16,68 @@ IndexController.definition = {
 
 /**
 * @see \App\Http\Controllers\Decks\IndexController::__invoke
- * @see app/Http/Controllers/Decks/IndexController.php:12
- * @route '/decks'
- */
+* @see app/Http/Controllers/Decks/IndexController.php:12
+* @route '/decks'
+*/
 IndexController.url = (options?: RouteQueryOptions) => {
     return IndexController.definition.url + queryParams(options)
 }
 
 /**
 * @see \App\Http\Controllers\Decks\IndexController::__invoke
- * @see app/Http/Controllers/Decks/IndexController.php:12
- * @route '/decks'
- */
+* @see app/Http/Controllers/Decks/IndexController.php:12
+* @route '/decks'
+*/
 IndexController.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
     url: IndexController.url(options),
     method: 'get',
 })
+
 /**
 * @see \App\Http\Controllers\Decks\IndexController::__invoke
- * @see app/Http/Controllers/Decks/IndexController.php:12
- * @route '/decks'
- */
+* @see app/Http/Controllers/Decks/IndexController.php:12
+* @route '/decks'
+*/
 IndexController.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
     url: IndexController.url(options),
     method: 'head',
 })
 
-    /**
+/**
 * @see \App\Http\Controllers\Decks\IndexController::__invoke
- * @see app/Http/Controllers/Decks/IndexController.php:12
- * @route '/decks'
- */
-    const IndexControllerForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-        action: IndexController.url(options),
-        method: 'get',
-    })
+* @see app/Http/Controllers/Decks/IndexController.php:12
+* @route '/decks'
+*/
+const IndexControllerForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: IndexController.url(options),
+    method: 'get',
+})
 
-            /**
+/**
 * @see \App\Http\Controllers\Decks\IndexController::__invoke
- * @see app/Http/Controllers/Decks/IndexController.php:12
- * @route '/decks'
- */
-        IndexControllerForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-            action: IndexController.url(options),
-            method: 'get',
-        })
-            /**
+* @see app/Http/Controllers/Decks/IndexController.php:12
+* @route '/decks'
+*/
+IndexControllerForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: IndexController.url(options),
+    method: 'get',
+})
+
+/**
 * @see \App\Http\Controllers\Decks\IndexController::__invoke
- * @see app/Http/Controllers/Decks/IndexController.php:12
- * @route '/decks'
- */
-        IndexControllerForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-            action: IndexController.url({
-                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
-                            _method: 'HEAD',
-                            ...(options?.query ?? options?.mergeQuery ?? {}),
-                        }
-                    }),
-            method: 'get',
-        })
-    
-    IndexController.form = IndexControllerForm
+* @see app/Http/Controllers/Decks/IndexController.php:12
+* @route '/decks'
+*/
+IndexControllerForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+    action: IndexController.url({
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'HEAD',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        }
+    }),
+    method: 'get',
+})
+
+IndexController.form = IndexControllerForm
+
 export default IndexController
--- resources/js/actions/App/Http/Controllers/Decks/index.ts
+++ resources/js/actions/App/Http/Controllers/Decks/index.ts
@@ -1,8 +1,9 @@
 import IndexController from './IndexController'
 import ShowController from './ShowController'
+
 const Decks = {
     IndexController: Object.assign(IndexController, IndexController),
-ShowController: Object.assign(ShowController, ShowController),
+    ShowController: Object.assign(ShowController, ShowController),
 }
 
 export default Decks
