1536a0745308341bf8ffacf1855fa52a25c7f7ec
Alec Ritson <hello@itsalec.co.uk>
2026-02-24 11:37:15 +0000

    WIP

--- resources/js/pages/decks/partials/DeckLeagues.vue
+++ resources/js/pages/decks/partials/DeckLeagues.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import MatchesTable from '@/components/matches/MatchesTable.vue';
 import PhantomBadge from '@/components/leagues/PhantomBadge.vue';
-
+import MatchesTable from '@/components/matches/MatchesTable.vue';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
 defineProps<{
     leagues: App.Data.Front.LeagueData[];
     archetypes: App.Data.Front.ArchetypeData[];
@@ -11,8 +10,8 @@ defineProps<{
 
 <template>
     <div class="space-y-4">
-        <Card class="gap-0 p-0 overflow-hidden" v-for="(league, idx) in leagues" :key="`league_${idx}`">
-            <CardHeader class="flex items-center p-2 justify-between">
+        <Card class="gap-0 overflow-hidden p-0" v-for="(league, idx) in leagues" :key="`league_${idx}`">
+            <CardHeader class="flex items-center justify-between p-2">
                 <span class="text-sm">{{ league.name }}</span>
                 <PhantomBadge v-if="league.phantom" />
             </CardHeader>
