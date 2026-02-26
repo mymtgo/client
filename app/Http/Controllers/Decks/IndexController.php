8d49b86cae53c27189759656a4c9cd77fb6863e4
Alec Ritson <hello@itsalec.co.uk>
2026-02-26 15:10:57 +0000

    WIP

--- app/Http/Controllers/Decks/IndexController.php
+++ app/Http/Controllers/Decks/IndexController.php
@@ -13,7 +13,7 @@ class IndexController
     {
         $decks = Deck::withCount(['wonMatches', 'lostMatches', 'matches'])
             ->withMax('matches', 'started_at')
-            ->whereHas('matches')
+
             ->orderByDesc('matches_max_started_at')
             ->get()
             ->map(fn ($deck) => DeckData::from($deck));
