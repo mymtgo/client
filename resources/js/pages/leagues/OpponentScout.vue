a0fe17ef35a8d72e64effaf63aee322dcbe4b6b0
Alec Ritson <hello@itsalec.co.uk>
2026-03-13 11:38:03 +0000

    Tweak overlay styling

--- resources/js/pages/leagues/OpponentScout.vue
+++ resources/js/pages/leagues/OpponentScout.vue
@@ -26,11 +26,9 @@ onUnmounted(() => {
 
 <template>
     <div class="h-screen" style="-webkit-app-region: drag">
-        <div class="flex h-full flex-col">
-            <OpponentScoutComponent v-if="opponent" :opponent="opponent" />
-            <div v-else class="flex items-center justify-center px-4 py-2 text-center text-sm opacity-50" style="color: #ffffff; background-color: #1a1a1a">
-                Waiting for match...
-            </div>
+        <OpponentScoutComponent v-if="opponent" :opponent="opponent" />
+        <div v-else class="flex h-full items-center justify-center px-4 text-center text-sm text-white opacity-50">
+            Waiting for match...
         </div>
     </div>
 </template>
