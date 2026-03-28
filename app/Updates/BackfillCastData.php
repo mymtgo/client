b69a59f9fb5ad41d7d57a558f44fa7023281aa11
Alec Ritson <hello@itsalec.co.uk>
2026-03-28 18:05:39 +0000

    Dispatch update on queue

--- app/Updates/BackfillCastData.php
+++ app/Updates/BackfillCastData.php
@@ -2,10 +2,7 @@
 
 namespace App\Updates;
 
-use App\Jobs\ComputeCardGameStats;
-use App\Models\MtgoMatch;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
+use App\Jobs\BackfillCardGameStats;
 
 class BackfillCastData extends AppUpdate
 {
@@ -16,18 +13,6 @@ class BackfillCastData extends AppUpdate
 
     public function run(): void
     {
-        // Clear existing stats so they get recomputed with cast data
-        DB::table('card_game_stats')->delete();
-
-        $matchIds = MtgoMatch::query()
-            ->where('state', 'complete')
-            ->whereNotNull('deck_version_id')
-            ->pluck('id');
-
-        foreach ($matchIds as $matchId) {
-            ComputeCardGameStats::dispatch($matchId);
-        }
-
-        Log::info("BackfillCastData: dispatched {$matchIds->count()} jobs");
+        BackfillCardGameStats::dispatch();
     }
 }
