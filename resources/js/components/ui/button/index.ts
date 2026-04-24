c02169cf2ed7efb142bd4bcf8b984a47b8d1940b
Alec Ritson <hello@itsalec.co.uk>
2026-04-24 14:24:58 +0100

    Tweaks

--- resources/js/components/ui/button/index.ts
+++ resources/js/components/ui/button/index.ts
@@ -2,6 +2,7 @@ import type { VariantProps } from "class-variance-authority"
 import { cva } from "class-variance-authority"
 
 export { default as Button } from "./Button.vue"
+export { default as ButtonLink } from "./ButtonLink.vue"
 
 export const buttonVariants = cva(
   "inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-md text-sm font-medium tracking-tight transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:stroke-[1.75] [&_svg:not([class*='size-'])]:size-[1em] shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
